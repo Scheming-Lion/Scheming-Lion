365b2e213d0ad230ee2b977925ca0cba8872a327
Adam Back <adb33@email.arizona.edu>
2014-10-16 15:56:06 -0700

    Tables created and ordered.

--- database.js
+++ database.js
@@ -1,81 +1,86 @@
 var Sequelize = require('sequelize');
 
 var Story = sequelize.define('Story', {
-  id: Sequelize.INTEGER
-  deleted: Sequelize.BOOLEAN,
+  id: {type: Sequelize.INTEGER, primarykey: true},
+  title: Sequelize.STRING,
   by: Sequelize.STRING,
   createdAt: Sequelize.INTEGER,
   text: Sequelize.STRING,
-  dead: Sequelize.BOOLEAN,
-  //figure out how to use array
-  kids: Sequelize.ARRAY(Sequelize.STRING),
   url: Sequelize.STRING,
   score: Sequelize.INTEGER,
-  title: Sequelize.STRING
+  //figure out how to use array
+  kids: Sequelize.ARRAY(Sequelize.STRING),
+  //optional below here?
+  deleted: Sequelize.BOOLEAN,
+  dead: Sequelize.BOOLEAN
 });
 
 var Comment = sequelize.define('Comment', {
-  id: Sequelize.INTEGER
-  deleted: Sequelize.BOOLEAN,
+  id: {type: Sequelize.INTEGER, primarykey: true},
   by: Sequelize.STRING,
+  parent: Sequelize.INTEGER,
   createdAt: Sequelize.INTEGER,
   text: Sequelize.STRING,
-  dead: Sequelize.BOOLEAN,
+  //optional below here
   //figure out how to use array
   kids: Sequelize.ARRAY(Sequelize.INTEGER),
   url: Sequelize.STRING,
   score: Sequelize.INTEGER,
   title: Sequelize.STRING
+  deleted: Sequelize.BOOLEAN,
+  dead: Sequelize.BOOLEAN,
 });
 
 var Job = sequelize.define('Job', {
-  id: Sequelize.INTEGER
-  deleted: Sequelize.BOOLEAN,
+  id: {type: Sequelize.INTEGER, primarykey: true},
+  title: Sequelize.STRING
   by: Sequelize.STRING,
   createdAt: Sequelize.INTEGER,
   text: Sequelize.STRING,
-  dead: Sequelize.BOOLEAN,
-  //figure out how to use array
-  kids: Sequelize.ARRAY(Sequelize.INTEGER),
   url: Sequelize.STRING,
   score: Sequelize.INTEGER,
-  title: Sequelize.STRING
+  //figure out how to use array
+  dead: Sequelize.BOOLEAN,
+  kids: Sequelize.ARRAY(Sequelize.INTEGER),
+  deleted: Sequelize.BOOLEAN
 });
 
 var Poll = sequelize.define('Poll', {
-  id: Sequelize.INTEGER
-  deleted: Sequelize.BOOLEAN,
+  id: {type: Sequelize.INTEGER, primarykey: true},
+  title: Sequelize.STRING,
   by: Sequelize.STRING,
   createdAt: Sequelize.INTEGER,
+  parts: Sequelize.ARRAY(Sequelize.INTEGER),
   text: Sequelize.STRING,
-  dead: Sequelize.BOOLEAN,
-  //figure out how to use array
-  kids: Sequelize.ARRAY(Sequelize.INTEGER),
   url: Sequelize.STRING,
   score: Sequelize.INTEGER,
-  title: Sequelize.STRING
+  //figure out how to use array
+  kids: Sequelize.ARRAY(Sequelize.INTEGER),
+  //optional
+  deleted: Sequelize.BOOLEAN,
+  dead: Sequelize.BOOLEAN,
 });
 
-var Pollopt = sequelize.define('Pollopt', {
-  id: Sequelize.INTEGER
-  deleted: Sequelize.BOOLEAN,
+var PollOption = sequelize.define('PollOption', {
+  id: {type: Sequelize.INTEGER, primarykey: true},
   by: Sequelize.STRING,
-  createdAt: Sequelize.INTEGER,
+  parent: Sequelize.INTEGER,
+  score: Sequelize.INTEGER,
   text: Sequelize.STRING,
-  dead: Sequelize.BOOLEAN,
+  createdAt: Sequelize.INTEGER,
   //figure out how to use array
   kids: Sequelize.ARRAY(Sequelize.INTEGER),
   url: Sequelize.STRING,
-  score: Sequelize.INTEGER,
-  title: Sequelize.STRING,
-  parts: Sequelize.ARRAY(Sequelize.INTEGER)
+  dead: Sequelize.BOOLEAN,
+  deleted: Sequelize.BOOLEAN,
+  title: Sequelize.STRING
 });
 
 var User = sequelize.define('User', {
   id: {type: Sequelize.STRING, unique: true, primarykey: true},
-  delay: Sequelize.INTEGER,
-  createdAt: Sequelize.INTEGER,
-  karma: Sequelize.INTEGER,
   about: Sequelize.STRING,
+  createdAt: Sequelize.INTEGER,
+  delay: Sequelize.INTEGER,
+  karma: Sequelize.INTEGER,
   submitted: Sequelize.ARRAY(Sequelize.INTEGER)
 });
