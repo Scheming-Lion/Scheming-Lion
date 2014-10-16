289a47dad8031df289c14d4eb0d199901c1aea46
Adam Back <adb33@email.arizona.edu>
2014-10-16 15:30:12 -0700

    Story table created.

--- database.js
+++ database.js
@@ -1,6 +1,17 @@
-var sequelize = require('sequelize');
+var Sequelize = require('sequelize');
 
 var Story = sequelize.define('Story', {
+  id: Sequelize.STRING,
+  deleted: Sequelize.BOOLEAN,
+  by: Sequelize.STRING,
+  createdAt: Sequelize.INTEGER,
+  text: Sequelize.STRING,
+  dead: Sequelize.BOOLEAN,
+  //figure out how to use array
+  kids: Sequelize.ARRAY(Sequelize.STRING),
+  url: Sequelize.STRING,
+  score: Sequelize.INTEGER,
+  title: Sequelize.STRING,
 });
 
 var Comment = sequelize.define('Comment', {
