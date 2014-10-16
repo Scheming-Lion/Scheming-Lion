edcb835d0f0e5e398a54b67ecc21cead8704a5b7
Adam Back <adb33@email.arizona.edu>
2014-10-16 15:24:58 -0700

    Basic tables defined.

--- database.js
+++ database.js
@@ -1,1 +1,19 @@
 var sequelize = require('sequelize');
+
+var Story = sequelize.define('Story', {
+});
+
+var Comment = sequelize.define('Comment', {
+});
+
+var Job = sequelize.define('Job', {
+});
+
+var Poll = sequelize.define('Poll', {
+});
+
+var Pollopt = sequelize.define('Pollopt', {
+});
+
+var User = sequelize.define('User', {
+});
