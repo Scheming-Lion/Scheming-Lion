d9d6cd0261ce60d804b82795b902c65375dc88e0
Justin Pinili <pinili.justin@gmail.com>
2014-10-16 17:24:46 -0700

    commented the server.js file and added folder for the dataFiles

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 
+// enables the views of the application to use straight HTML files.
+// essentially we are using ejs as the template for our server's views
+// but not really using ejs in the files at all.
 app.engine('html', require('ejs').renderFile);
-
 app.set('view engine', 'html');
+
+// tells the application where to go to find the views so that they can
+// be rendered.
 app.set('views', __dirname + '/client');
 
+// tells the application where to go to find static files.
 app.use(express.static(__dirname + './../client'));
 
+// route for the home page.
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.get('/importData', function(req, res) {
+  // will need to fsReadfile
 });
 
 module.exports = app;
