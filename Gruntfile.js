df859081be283d9246e2d030feaf6f786e76c1af
Adam Back <adb33@email.arizona.edu>
2014-10-20 15:46:02 -0700

    (test) Removed items from Gruntfile not necessary to testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,30 +2,6 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      dist: {
-        src: [
-          // './public/lib/*.js',libraries
-          './public/client/app.js',// All JS in the libs folder
-          './public/client/link.js',// All JS in the libs folder
-          './public/client/links.js',// All JS in the libs folder
-          './public/client/linkView.js',// All JS in the libs folder
-          './public/client/linksView.js',// All JS in the libs folder
-          './public/client/createLinkView.js',// All JS in the libs folder
-          './public/client/router.js'// All JS in the libs folder
-        ],
-        dest: './public/dist/production.js'
-      },
-      libs: {
-        src: [
-          './public/lib/underscore.js',
-          './public/lib/jquery.js',
-          './public/lib/backbone.js',
-          './public/lib/handlebars.js'
-        ],
-        dest: './public/dist/libraries.js'
-      },
-    },
 
     mochaTest: {
       test: {
@@ -42,65 +18,19 @@ module.exports = function(grunt) {
       }
     },
 
-    uglify: {
-      dist: {
-        src: './public/dist/production.js',
-        dest: './public/dist/production.min.js'
-      },
-      libs: {
-        src: './public/dist/libraries.js',
-        dest: './public/dist/libraries.min.js'
-      }
-    },
-
-    jshint: {
-      files: [
-        'public/client/*.js'
-      ],
-      options: {
-        force: 'false',
-        jshintrc: '.jshintrc',
-        ignores: [
-          'public/lib/**/*.js',
-          'public/dist/**/*.js'
-        ]
-      }
-    },
-
-    cssmin: {
-    },
-
     watch: {
       scripts: {
         files: [
           'public/client/**/*.js',
           'public/lib/**/*.js',
         ],
-        tasks: [
-          'concat',
-          'uglify'
-        ]
-      },
-      css: {
-        files: 'public/*.css',
-        tasks: ['cssmin']
+        tasks: ['mochaTest']
       }
-    },
-
-    shell: {
-      prodServer: {
-        command: 'git push azure master'
-      }
-    },
+    }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
 
   grunt.registerTask('server-dev', function (target) {
@@ -120,27 +50,15 @@ module.exports = function(grunt) {
   // Main grunt tasks
   ////////////////////////////////////////////////////
 
-  grunt.registerTask('test', [
-    'mochaTest'
-  ]);
+  grunt.registerTask('test', ['mochaTest']);
 
-  grunt.registerTask('build', ['jshint', "mochaTest", 'concat', 'uglify']);
+  grunt.registerTask('build', []);
 
   grunt.registerTask('deploy', function(n) {
-
     if(grunt.option('prod')) {
-      grunt.task.run(['build']);
-      grunt.task.run(['shell']);
       // add your production server task here
     } else {
-      grunt.task.run(['build']);
-      grunt.task.run([ 'server-dev' ]);
+      // add your dev server task here
     }
   });
-
-  // grunt.registerTask('deploy', [
-  //   // add your deploy tasks here
-  // ]);
-
-
 };
