dd9229bafb1b94688d143ba269e53ff3934b8cfb
Adam Back <adb33@email.arizona.edu>
2014-10-20 16:33:34 -0700

    (test/refactor) Removed istanbul as testing coverage for replacement with blanketJS. Running \'grunt test\' now shows tests in terminal and outputs a coverage html in the testing folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,17 @@ module.exports = function(grunt) {
           reporter: 'spec'
         },
         src: ['testing/**/*.js']
+      },
+      coverage: {
+        options: {
+          reporter: 'html-cov',
+          // use the quiet flag to suppress the mocha console output
+          quiet: true,
+          // specify a destination file to capture the mocha
+          // output (the quiet option does not suppress this)
+          captureFile: 'testing/coverage.html'
+        },
+        src: ['testing/**/*.js']
       }
     },
 
