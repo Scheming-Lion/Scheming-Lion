4a1459cb0512d14a979bac9446408a341e4f032c
Oleg <o.yanchinskiy@gmail.com>
2014-10-21 13:19:19 -0700

    Test results for Top Stories showing up on dashboard.  Working on integrating Angular.

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,24 @@
-var app = angular.module('myModule', ['ui.bootstrap']);
+var app = angular.module('myApp', ["ui.router"]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/state1");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('state1', {
+      url: "../partials/state1.html",
+      templateUrl: "../partials/state1.html",
+      controller: function( $scope ) {
+        $scope.things = ['hello','oleg'];
+      }
+    })
+    .state('state2', {
+    	url: "../partials/state2.html",
+    	templateUrl: "../partials/state2.html",
+    	controller: function( $scope ){
+    		$scope.things = ['beuenos', 'dias', 'oleg'];
+    	}
+    })
+});
