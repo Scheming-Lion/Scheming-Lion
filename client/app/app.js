3a3f5ecb3bd366ded72e15402bdf1ff46035bd37
Oleg <o.yanchinskiy@gmail.com>
2014-10-21 14:17:53 -0700

    deleted extra directory, working on search template

--- client/app/app.js
+++ client/app/app.js
@@ -3,22 +3,23 @@ var app = angular.module('myApp', ["ui.router"]);
 app.config(function($stateProvider, $urlRouterProvider) {
   //
   // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/state1");
+  $urlRouterProvider.otherwise("/state2");
   //
   // Now set up the states
   $stateProvider
-    .state('state1', {
-      url: "../partials/state1.html",
-      templateUrl: "../partials/state1.html",
-      controller: function( $scope ) {
-        $scope.things = ['hello','oleg'];
-      }
-    })
     .state('state2', {
-    	url: "../partials/state2.html",
-    	templateUrl: "../partials/state2.html",
-    	controller: function( $scope ){
-    		$scope.things = ['beuenos', 'dias', 'oleg'];
-    	}
+    	views : {
+	    	'searchTopStories': {
+	    		template: "<input type='text' class='userSearch' placeholder='Search for top user stories...''>"
+	    	}
+	    }
     })
 });
+
+// $stateProvider.state("home", {
+//     views: {
+//         "main": {
+//             template: "<h1>HELLO!</h1>"
+//         }
+//     }    
+// })
