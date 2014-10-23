cb97de6374c146b3974a2f1fc1c900bf1e7f83a3
Adam Back <adb33@email.arizona.edu>
2014-10-23 15:54:07 -0700

    (refactor) The credential information for the database are now stored in a local databaseCredential.js file which is ignored by git and will never be committed. The file databaseConfig.example.js shows devs how to do this on their own local machine.

---
+++ server/database/databaseConfig.example.js
@@ -0,0 +1,12 @@
+// To store your own database credentials, create a file named
+// databaseConfig.js and put the code from below into it.
+// Modify per your credentials. Your credentials will not be added
+// to GitHub because server/database/databaseConfig.js has been
+// added to the .gitignore file, but database.js will have 
+// access to the credentials locally.
+
+module.exports = {
+  databaseName: "ChocolateDB",
+  username: "WillieWonka",
+  password: "Snozberries"  
+};
