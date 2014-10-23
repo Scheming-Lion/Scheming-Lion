aa65192fdc2e05a848bd14115a4b3556fe50a0bd
Justin Pinili <pinili.justin@gmail.com>
2014-10-23 11:34:03 -0700

    (feat) able to grab the titles from the top 100 stories.

--- client/topStories/topStories.js
+++ client/topStories/topStories.js
@@ -30,11 +30,15 @@ $.get(url + "/user/" + username + ".json", function(userProfile, status) {
           var story = "<div class='story'>" + '<strong>Title: </strong>'+  topStories[i].title + '<br>' +
                       "<strong>Score: </strong>" + topStories[i].score + '<br>' +
                       "<strong>Link: </strong>" + topStories[i].url +'<br>'+'<br>' + "</div>";
+<<<<<<< HEAD
 <<<<<<< HEAD:client/topStories/topStories.js
           // $('.view').append(story);
 =======
           $('.view').append(story);
 >>>>>>> (feat) re-organized some files on the front end. also started top100 feature.:client/topStories/topStories.js
+=======
+          // $('.view').append(story);
+>>>>>>> (feat) able to grab the titles from the top 100 stories.
         }
       }
     });
