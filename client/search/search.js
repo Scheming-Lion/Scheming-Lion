fd9fb9531fec221c0a8b0245cf4ae6572de6917a
Oleg <o.yanchinskiy@gmail.com>
2014-10-25 16:27:42 -0700

    (FEAT) Implemented spinner in the search

--- client/search/search.js
+++ client/search/search.js
@@ -7,39 +7,41 @@ angular.module('myApp.search', [] )
 		searchObject.topStories = {
 			data:[]
 		};
-
-		searchObject.fetchData = function(userName){
-			console.log('Username', userName)
-			$http({
-				method: 'GET',
-				url: searchObject.url + "/user/" + userName + ".json",
-			}).success(function(userProfile, status){
-				searchObject.storyIds = userProfile.submitted;
-				var searchItems = [];
-				searchObject.storyIds.forEach(function(storyId){
-					$http({
-						method: 'GET',
-						url: searchObject.url + "/item/" + storyId + ".json",
-						})
-						.success(function(story, status){
-						searchItems.push(story);
-						if(searchItems.length === searchObject.storyIds.length){
-							var stories = searchItems.filter(function(story){
-								return story.score;
+			searchObject.fetchData = function(userName){	
+				return new Promise(function(resolve, reject){
+				console.log('Username', userName)
+				$http({
+					method: 'GET',
+					url: searchObject.url + "/user/" + userName + ".json",
+				}).success(function(userProfile, status){
+					searchObject.storyIds = userProfile.submitted;
+					var searchItems = [];
+					searchObject.storyIds.forEach(function(storyId){
+						$http({
+							method: 'GET',
+							url: searchObject.url + "/item/" + storyId + ".json",
 							})
-							var sortedStories = stories.sort(searchObject.sort);
-							var topStoriesArray = sortedStories.slice(0,10);
-							searchObject.topStories.data = topStoriesArray;
-
-							console.log(searchObject.topStories.data);
-							return searchObject.topStories.data;
-						}
-					})
-				}) 
-			})
-			.error(function(error){
-				console.log(error)
-			})
+							.success(function(story, status){
+							searchItems.push(story);
+							if(searchItems.length === searchObject.storyIds.length){
+								var stories = searchItems.filter(function(story){
+									return story.score;
+								})
+								var sortedStories = stories.sort(searchObject.sort);
+								var topStoriesArray = sortedStories.slice(0,10);
+								searchObject.topStories.data = topStoriesArray;
+								console.log(searchObject.topStories.data);		
+								// resolve(searchObject.topStories.data);
+								resolve();
+							}
+						})
+					}) 
+				})
+				.error(function(error){
+					console.log(error);
+					reject(error);
+				});
+			});
 		}
 
 		searchObject.sort = function(a, b) {
@@ -53,19 +55,20 @@ angular.module('myApp.search', [] )
 	})
 	
 
-	.controller('searchCtrl', function($scope, search, $location){
-		// Currently the $location(path) is not working 
-		// $scope.go = function( path ){
-		// 	console.log( path );
-		// 	$location.path( path ); 
-		// };
-
+	.controller('searchCtrl', function($scope, search){
 		$scope.factory = search;
-		// $scope.factory.topStories = search.topStories;
+		$scope.loading = false; 
 		$scope.topStories = search.topStories;
 		$scope.searchUserName = function(userName){
-			search.fetchData(userName);
-			$scope.userName = '';
+			$scope.loading = true; 
+			search.fetchData(userName).then(function(){
+				$scope.$apply(function(){
+					$scope.loading = false;
+					$scope.userName = '';
+				});
+			}).catch(function(error){
+				console.log(error);
+			})
 		};
 
 		$scope.storiesArray = search.topStories;
--- server/database/databaseConfig.example.js
+++ server/database/databaseConfig.example.js
@@ -6,7 +6,7 @@
 // access to the credentials locally.
 
 module.exports = {
-  databaseName: "ChocolateDB",
-  username: "WillieWonka",
-  password: "Snozberries"
+  databaseName: "lionbase",
+  username: "bc8fa3955e6d18",
+  password: "71b12f9d5436d6f"
 };
