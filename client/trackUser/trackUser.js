54107be2a34e032b6320318e84cbe2730de46280
Oleg <o.yanchinskiy@gmail.com>
2014-10-27 14:44:24 -0700

    (FEAT) Track post is not working

--- client/trackUser/trackUser.js
+++ client/trackUser/trackUser.js
@@ -1,17 +1,20 @@
 angular.module('myApp.trackUser', [] )
 
 	.controller('trackUserController', function($scope){
-		$scope.userToTrack;
-		$scope.userKarma;
-		$scope.userSubmitted;
 		// var userData = $firebase();
 		$scope.trackUser = function(user){
+			$scope.userToTrack;
+			$scope.userKarma;
+			$scope.userSubmitted;
+			$scope.show = false;
+			$scope.loading = true;
 			var url = "https://hacker-news.firebaseio.com/v0/user/";
 			var fullUrl = url.concat(user);
 			var hackerNewsRef = new Firebase(fullUrl);
 			hackerNewsRef.on('value', function (snapshot) {
 				console.log('updated', snapshot);
-				$scope.userToTrack= snapshot.val().id;
+				$scope.loading = false;
+				$scope.show = true;
 				$scope.userSubmitted = snapshot.val().submitted;
 				$scope.userKarma = snapshot.val().karma;
 			}, function (errorObject) {
