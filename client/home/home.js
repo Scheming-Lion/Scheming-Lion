1788950ba815d957b531871bd86047d3676b0d5c
Justin Pinili <pinili.justin@gmail.com>
2014-10-27 16:49:28 -0700

    (fix) only displays bar graph after three categories are retrieved.

--- client/home/home.js
+++ client/home/home.js
@@ -4,17 +4,21 @@ angular.module('myApp.home', [])
     
     $scope.total = [];
 
-    $scope.getTitle = function(title, last) {
+    $scope.getTitle = function(title, callback) {
       $http.post('http://scheminglion.azurewebsites.net/findTotal', { search: title })
         .success(function(data) {
           var text = data.num_rows.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
           $scope.total.push([title, data.num_rows, text]);
-          if (last) { totalGraph.link($scope); }
+          callback();
         });
     };
 
-    $scope.getTitle('stories', false);
-    $scope.getTitle('comments', false);
-    $scope.getTitle('users', true);
+    $scope.getTitle('users', function() {
+      $scope.getTitle('stories', function() {
+        $scope.getTitle('comments', function() {
+          totalGraph.link($scope);
+        });
+      });
+    });
 	});
