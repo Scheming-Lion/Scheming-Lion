81d6958117daa38dd3c18fbae2c78577dfd33d33
Oleg <o.yanchinskiy@gmail.com>
2014-10-27 14:27:25 -0700

    (FEAT) Track post working, still has a BUG in the way items are displayed

--- client/trackPost/trackPost.js
+++ client/trackPost/trackPost.js
@@ -1,5 +1,32 @@
 angular.module('myApp.trackPost', [] )
 
 	.controller('trackPostController', function($scope){
+		$scope.postToTrack;
+		// $scope.userSubmitted;
+		$scope.trackPost = function(url){
+			$scope.loading = true;
+			$scope.show = false; 
+			$scope.postAuthor = '';
+			$scope.createdAt = '';
+			$scope.text = '';
+			var url = "https://hacker-news.firebaseio.com/v0/item/";
+			var postId = $scope.postToTrack;
+			var postRef = new Firebase(url + postId);
+			// Attach an asynchronous callback to read the data at our post reference
+			postRef.on('value', function (snapshot) {
+			  console.log(snapshot.val());
+			  $scope.loading = false;
+				$scope.show = true; 
+			  $scope.postAuthor = snapshot.val().by;
+			  var time = snapshot.val().time;
+			  var date = new Date(0);
+			  $scope.createdAt = date.setUTCSeconds(time);
+			  $scope.text = snapshot.val().text;
+			}, function (errorObject) {
+			  console.log('The read failed: ' + errorObject.code);
+			});
+		}
 
 	});
+
+
