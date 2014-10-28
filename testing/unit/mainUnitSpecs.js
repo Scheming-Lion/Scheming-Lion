edcca800b1bd0e24a5c6adcd723195913b90e80d
Adam Back <adb33@email.arizona.edu>
2014-10-27 17:03:43 -0700

    (test) Wrote tests for main.js for when state dependency gets resolve.

--- testing/unit/mainUnitSpecs.js
+++ testing/unit/mainUnitSpecs.js
@@ -1,49 +1,62 @@
-'use strict'
-
 describe('mainController in main.js', function() {
-   // Set up the module
-  beforeEach( module( 'myApp.main') );
-  beforeEach( module ( 'ui.router' ) );
-  // Set up the views
-
-  var ctrl, scope, state;
-
+  
+  var $mainController, $scope;
  
-  beforeEach(inject(function($controller, $rootScope, $state) {
+  beforeEach( function() {
+    module( 'myApp.main' );
+  });
+
+  beforeEach(function() {inject(function(_$controller_, _$rootScope_) {
     // Create a new scope that's a child of the $rootScope
-    scope = $rootScope.$new();
-    state = $state;
+    $scope = _$rootScope_.$new();
+
     // Create the controller
-    ctrl = $controller('mainController', {
-      '$scope': scope
+    $mainController = _$controller_('mainController', {
+      "$scope": $scope
     });
 
-  }));
+  })});
 
-  it('should transition to main.course', inject(function ($state, $rootScope) {
+// The tests below do not run due to some issue with ui.router
+// and missing $stateProvider issues. As a result, the test below
+// have been marked as pending. The will pass if the issue with
+// state is corrected.
+
+// This may be helpful for getting the state issues to run
     // $state.transitionTo('main.subviews');
     // $rootScope.$apply();
     // expect($state.current.name).toBe('main.subviews');
-  }));
 
-  xit('should have a search property set to false', function() {
-    expect(true).to.be.true;
+  xit('should have a function named chooseView', function () {
+    expect($scope.chooseView).to.exist;
+    expect($scope.chooseView).to.be.a('function');
   });
 
-  xit('should grab ids for the top 100 stories on HN', function() {
-    expect(expecation).to.equal(equal);
+  xit('should have a function named primary', function () {
+    expect($scope.primary).to.exist;
+    expect($scope.primary).to.be.a('function');
   });
 
-  xit('should count words in titles', function() {
-    expect(expecation).to.equal(equal);
+  xit('should have current view set as \'search\' on page load', function() {
+    expect($scope.current).to.equal('search');
   });
 
-  xit('should filter common words out of the word cloud', function() {
-    expect(expecation).to.equal(equal);
+  xit('should choose a different view', function() {
+    expect($scope.current).to.equal('search');
+    $scope.chooseView('main');
+    expect($scope.current).to.equal('main');
   });
 
-  xit('should give words to D3 for rendering', function() {
-    expect(expecation).to.equal(equal);
+  xit('primary should return false if $scope.current is undefined', function() {
+    $scope.chooseView(undefined);
+    expect($scope.current).to.be.undefined;
+    expect($scope.primary('test')).to.be.false;
+  });
+
+  xit('primary should return false if $scope.current is undefined', function() {
+    $scope.chooseView('justin');
+    expect($scope.current).to.equal('justin');
+    expect($scope.primary('justin')).to.be.true;
   });
 
 });
